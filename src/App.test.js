adac083b7956896021e14924be51be44ef055e10
Yifan Ai <ayfallen@hotmail.com>
2020-11-16 23:48:04 +1100

    Edit the basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Dental Chatbot title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Dental Chatbot/i);
   expect(linkElement).toBeInTheDocument();
 });
